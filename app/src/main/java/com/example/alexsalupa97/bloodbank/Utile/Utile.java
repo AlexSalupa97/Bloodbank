a36c0786a339adcca55582707709e5597041846f
alexsalupa97 <alexsalupa97@gmail.com>
2019-02-19 22:31:22 +0200

    Profil

--- app/src/main/java/com/example/alexsalupa97/bloodbank/Utile/Utile.java
+++ app/src/main/java/com/example/alexsalupa97/bloodbank/Utile/Utile.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class Utile {
 
     public static String fisier = "SharedPreferences";
-    public static String URL="http://3025268f.ngrok.io/ProiectLicentaBloodbank/webresources/";
+    public static String URL="http://2b2f1c58.ngrok.io/ProiectLicentaBloodbank/webresources/";
 
     public static ArrayList<Intrebari> intrebari;
     public static ArrayList<Compatibilitati> compatibilitati;
