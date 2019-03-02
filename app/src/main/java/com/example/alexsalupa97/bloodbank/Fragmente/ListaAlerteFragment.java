09be76c2f38e3f1abaddc8d69e3924074ddd84b5
alexsalupa97 <alexsalupa97@gmail.com>
2019-03-02 21:43:05 +0200

    RecyclerView alerte per total

--- app/src/main/java/com/example/alexsalupa97/bloodbank/Fragmente/ListaAlerteFragment.java
+++ app/src/main/java/com/example/alexsalupa97/bloodbank/Fragmente/ListaAlerteFragment.java
@@ -3,17 +3,21 @@ package com.example.alexsalupa97.bloodbank.Fragmente;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.example.alexsalupa97.bloodbank.Adaptoare.AdaptorAlerteRV;
 import com.example.alexsalupa97.bloodbank.Clase.CTS;
 import com.example.alexsalupa97.bloodbank.Clase.CantitatiCTS;
 import com.example.alexsalupa97.bloodbank.Clase.Compatibilitati;
 import com.example.alexsalupa97.bloodbank.Clase.GrupeSanguine;
 import com.example.alexsalupa97.bloodbank.Clase.LimiteCTS;
 import com.example.alexsalupa97.bloodbank.R;
+import com.example.alexsalupa97.bloodbank.RecyclerViewOrizontal.ItemModelAlerte;
+import com.example.alexsalupa97.bloodbank.RecyclerViewOrizontal.SectionModelAlerte;
 import com.example.alexsalupa97.bloodbank.Utile.Utile;
 
 import java.util.ArrayList;
@@ -31,8 +35,12 @@ public class ListaAlerteFragment extends Fragment {
     Map<CTS, Map<GrupeSanguine, Integer>> mapLimitePerCTSPerGrupa;
 
     ArrayList<CantitatiCTS> listaCantitatiCTS;
+    Map<CTS,ArrayList<CantitatiCTS>> mapCantitatiPerCTS;
 
-    TextView tvDetaliiLimite;
+    RecyclerView rvAlerte;
+    ArrayList<SectionModelAlerte> sectiuni;
+
+
 
 
     public ListaAlerteFragment() {
@@ -46,10 +54,10 @@ public class ListaAlerteFragment extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_lista_alerte, container, false);
 
-        tvDetaliiLimite = (TextView) rootView.findViewById(R.id.tvDetaliiLimite);
-        tvDetaliiLimite.setText("");
 
-        listaCantitatiCTS=new ArrayList<>();
+
+        rvAlerte =(RecyclerView)rootView.findViewById(R.id.rvAlerte);
+
 
 
         try {
@@ -65,21 +73,24 @@ public class ListaAlerteFragment extends Fragment {
                 mapLimitePerCTSPerGrupa.put(cts, mapIntermediar);
             }
 
+            mapCantitatiPerCTS = new HashMap<>();
+
             for (CTS cts : mapCantitatiDisponibilePerCTSPerGrupa.keySet()) {
                 String deAfisat = "\n\n";
-
 
 
                 Map<GrupeSanguine, Integer> mapCantitatiDisponibile = mapCantitatiDisponibilePerCTSPerGrupa.get(cts);
                 Map<GrupeSanguine, Integer> mapLimite = mapLimitePerCTSPerGrupa.get(cts);
 
 
-                deAfisat += "\t"+cts.getNumeCTS();
+                deAfisat += "\t" + cts.getNumeCTS();
+
+                listaCantitatiCTS = new ArrayList<>();
 
                 for (Compatibilitati grupaSanguinaDonator : Utile.compatibilitati) {
                     if (Utile.preluareGrupaSanguina(getActivity()).equals(grupaSanguinaDonator.getGrupaSanguinaDonatoare().getGrupaSanguina()))
                         try {
-                            CantitatiCTS cantitateCTSCurent=new CantitatiCTS();
+                            CantitatiCTS cantitateCTSCurent = new CantitatiCTS();
                             cantitateCTSCurent.setCts(cts);
                             cantitateCTSCurent.setGrupaSanguina(grupaSanguinaDonator.getGrupaSanguinaReceiver());
                             cantitateCTSCurent.setCantitateDisponibilaML(mapCantitatiDisponibile.get(grupaSanguinaDonator.getGrupaSanguinaReceiver()));
@@ -95,10 +106,33 @@ public class ListaAlerteFragment extends Fragment {
 
                 }
 
-                tvDetaliiLimite.setText(tvDetaliiLimite.getText() + deAfisat);
-
-
+                mapCantitatiPerCTS.put(cts, listaCantitatiCTS);
             }
+
+
+        sectiuni=new ArrayList<>();
+
+        for(CTS cts:mapCantitatiPerCTS.keySet()) {
+            SectionModelAlerte dm = new SectionModelAlerte();
+
+            dm.setTitlu(cts.getNumeCTS());
+
+            ArrayList<ItemModelAlerte> itemeInSectiune = new ArrayList<>();
+            for (CantitatiCTS cantitatiCTS : mapCantitatiPerCTS.get(cts)) {
+                itemeInSectiune.add(new ItemModelAlerte(cantitatiCTS));
+            }
+
+            dm.setItemeInSectiune(itemeInSectiune);
+
+            sectiuni.add(dm);
+        }
+
+            rvAlerte.setHasFixedSize(true);
+
+            AdaptorAlerteRV adapter = new AdaptorAlerteRV(getActivity(), sectiuni);
+            rvAlerte.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+            rvAlerte.setAdapter(adapter);
+
 
         } catch (Exception ex) {
 
