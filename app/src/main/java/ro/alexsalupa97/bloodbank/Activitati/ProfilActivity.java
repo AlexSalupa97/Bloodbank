30fc9dec3052842e0a1b4bdf1a997d0e77773faa
alexsalupa97 <alexsalupa97@gmail.com>
2019-05-03 05:39:12 +0300

    design donator

--- app/src/main/java/ro/alexsalupa97/bloodbank/Activitati/ProfilActivity.java
+++ app/src/main/java/ro/alexsalupa97/bloodbank/Activitati/ProfilActivity.java
@@ -1,11 +1,18 @@
 package ro.alexsalupa97.bloodbank.Activitati;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ro.alexsalupa97.bloodbank.Adaptoare.AdaptorIstoricDonatiiRV;
 import ro.alexsalupa97.bloodbank.Clase.IstoricDonatii;
@@ -28,19 +35,36 @@ public class ProfilActivity extends AppCompatActivity {
     TextView tvOras;
     ImageView ivStareAnalize;
 
+    Dialog dialogInfo;
+    Button btnOK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
+        getSupportActionBar().setElevation(0);
 
-        tvNume=(TextView)findViewById(R.id.tvNume);
-        tvTelefon=(TextView)findViewById(R.id.tvTelefon);
-        tvEmail=(TextView)findViewById(R.id.tvEmail);
-        tvGrupaSanguina=(TextView)findViewById(R.id.tvGrupaSanguina);
-        tvOras=(TextView)findViewById(R.id.tvOras);
-        ivStareAnalize=(ImageView)findViewById(R.id.ivStareAnalize);
+        dialogInfo = new Dialog(ProfilActivity.this);
+        dialogInfo.setContentView(R.layout.dialog_info);
+
+        btnOK = (Button) dialogInfo.findViewById(R.id.btnDialogOK);
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogInfo.dismiss();
+            }
+        });
+
+
+        tvNume = (TextView) findViewById(R.id.tvNume);
+        tvTelefon = (TextView) findViewById(R.id.tvTelefon);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvGrupaSanguina = (TextView) findViewById(R.id.tvGrupaSanguina);
+        tvOras = (TextView) findViewById(R.id.tvOras);
+        ivStareAnalize = (ImageView) findViewById(R.id.ivStareAnalize);
 
         tvNume.setText(Utile.preluareUsername(getApplicationContext()));
         tvTelefon.setText(Utile.preluareTelefon(getApplicationContext()));
@@ -48,26 +72,24 @@ public class ProfilActivity extends AppCompatActivity {
         tvGrupaSanguina.setText(Utile.preluareGrupaSanguina(getApplicationContext()));
         tvOras.setText(Utile.preluareOras(getApplicationContext()));
 
-        if(Utile.preluareStareAnalize(getApplicationContext()).equals("ok"))
+        if (Utile.preluareStareAnalize(getApplicationContext()).equals("ok"))
             ivStareAnalize.setImageResource(R.drawable.good);
-        else
-            if(Utile.preluareStareAnalize(getApplicationContext()).equals("!ok"))
-                ivStareAnalize.setImageResource(R.drawable.bad);
-            else
-                if(Utile.preluareStareAnalize(getApplicationContext()).equals("neefectuate"))
-                    ivStareAnalize.setImageResource(R.drawable.not_done);
+        else if (Utile.preluareStareAnalize(getApplicationContext()).equals("!ok"))
+            ivStareAnalize.setImageResource(R.drawable.bad);
+        else if (Utile.preluareStareAnalize(getApplicationContext()).equals("neefectuate"))
+            ivStareAnalize.setImageResource(R.drawable.not_done);
 
-        sectiuni=new ArrayList<>();
+        sectiuni = new ArrayList<>();
 
         SectionModelIstoric dm = new SectionModelIstoric();
 
         dm.setTitlu("Istoric donatii");
 
         ArrayList<ItemModelIstoric> itemeInSectiune = new ArrayList<ItemModelIstoric>();
-        for (IstoricDonatii id:Utile.listaIstoricDonatii) {
-            int index=id.getDataDonatie().indexOf("T");
-            String substring=id.getDataDonatie().substring(0,index);
-            itemeInSectiune.add(new ItemModelIstoric(substring,id.getCantitateDonataML()+"ml"));
+        for (IstoricDonatii id : Utile.listaIstoricDonatii) {
+            int index = id.getDataDonatie().indexOf("T");
+            String substring = id.getDataDonatie().substring(0, index);
+            itemeInSectiune.add(new ItemModelIstoric(substring, id.getCantitateDonataML() + "ml"));
         }
 
         dm.setItemeInSectiune(itemeInSectiune);
@@ -104,5 +126,19 @@ public class ProfilActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.meniu_info_donator, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.item_info)
+            dialogInfo.show();
+        return super.onOptionsItemSelected(item);
     }
 }
