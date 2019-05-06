c32e05d962a119d56611ea191bccf9965898da7f
alexsalupa97 <alexsalupa97@gmail.com>
2019-05-06 05:27:31 +0300

    bullet text view

--- app/src/main/java/ro/alexsalupa97/bloodbank/Activitati/TestActivity.java
+++ app/src/main/java/ro/alexsalupa97/bloodbank/Activitati/TestActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import ro.alexsalupa97.bloodbank.Adaptoare.AdaptorExpandableLV;
 import ro.alexsalupa97.bloodbank.R;
+import ro.alexsalupa97.bloodbank.ViewCustom.BulletTextView;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -20,109 +22,15 @@ import java.util.List;
 
 public class TestActivity extends AppCompatActivity {
 
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
-
-    private static final int INTERVAL = 15;
-    private static final DecimalFormat FORMATTER = new DecimalFormat("00");
-
-    private TimePicker picker; // set in onCreate
-    private NumberPicker minutePicker;
+    TextView tvBullet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.lvExp);
-
-        // preparing list data
-        prepareListData();
-
-        listAdapter = new AdaptorExpandableLV(this, listDataHeader, listDataChild);
-
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
-    }
-
-    /*
-     * Preparing the list data
-     */
-    private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
-        // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
-
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
-
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
-
-        picker=(TimePicker)findViewById(R.id.tpProgramare);
-        picker.setIs24HourView(true);
-
-        setMinutePicker();
-
-        picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                Toast.makeText(getApplicationContext(),hourOfDay+":"+getMinute(),Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    public void setMinutePicker() {
-        int numValues = 60 / INTERVAL;
-        String[] displayedValues = new String[numValues];
-        for (int i = 0; i < numValues; i++) {
-            displayedValues[i] = FORMATTER.format(i * INTERVAL);
-        }
-
-        View minute = picker.findViewById(Resources.getSystem().getIdentifier("minute", "id", "android"));
-        if ((minute != null) && (minute instanceof NumberPicker)) {
-            minutePicker = (NumberPicker) minute;
-            minutePicker.setMinValue(0);
-            minutePicker.setMaxValue(numValues - 1);
-            minutePicker.setDisplayedValues(displayedValues);
-        }
-    }
-
-    public int getMinute() {
-        if (minutePicker != null) {
-            return (minutePicker.getValue() * INTERVAL);
-        } else {
-            return picker.getCurrentMinute();
-        }
+        tvBullet=(TextView)findViewById(R.id.tvBullet);
+        CharSequence bulletedList = BulletTextView.makeBulletList(100,"First line", "Second line", "Really long third line that will wrap and indent properly.");
+        tvBullet.setText(bulletedList);
     }
 }
---
+++ app/src/main/java/ro/alexsalupa97/bloodbank/ViewCustom/BulletTextView.java
@@ -0,0 +1,66 @@
+package ro.alexsalupa97.bloodbank.ViewCustom;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.BulletSpan;
+
+public class BulletTextView {
+
+    /**
+     * Returns a CharSequence containing a bulleted and properly indented list.
+     *
+     * @param leadingMargin In pixels, the space between the left edge of the bullet and the left edge of the text.
+     * @param context
+     * @param stringArrayResId A resource id pointing to a string array. Each string will be a separate line/bullet-point.
+     * @return
+     */
+    public static CharSequence makeBulletListFromStringArrayResource(int leadingMargin, Context context, int stringArrayResId) {
+        return makeBulletList(leadingMargin, context.getResources().getStringArray(stringArrayResId));
+    }
+
+    /**
+     * Returns a CharSequence containing a bulleted and properly indented list.
+     *
+     * @param leadingMargin In pixels, the space between the left edge of the bullet and the left edge of the text.
+     * @param context
+     * @param linesResIds An array of string resource ids. Each string will be a separate line/bullet-point.
+     * @return
+     */
+    public static CharSequence makeBulletListFromStringResources(int leadingMargin, Context context, int... linesResIds) {
+        int len = linesResIds.length;
+        CharSequence[] cslines = new CharSequence[len];
+        for (int i = 0; i < len; i++) {
+            cslines[i] = context.getString(linesResIds[i]);
+        }
+        return makeBulletList(leadingMargin, cslines);
+    }
+
+    /**
+     * Returns a CharSequence containing a bulleted and properly indented list.
+     *
+     * @param leadingMargin In pixels, the space between the left edge of the bullet and the left edge of the text.
+     * @param lines An array of CharSequences. Each CharSequences will be a separate line/bullet-point.
+     * @return
+     */
+    public static CharSequence makeBulletList(int leadingMargin, CharSequence... lines) {
+        SpannableStringBuilder sb = new SpannableStringBuilder();
+        for (int i = 0; i < lines.length; i++) {
+            CharSequence line = lines[i] + (i < lines.length-1 ? "\n" : "");
+            Spannable spannable = new SpannableString(line);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                spannable.setSpan(new BulletSpan(leadingMargin, Color.parseColor("#B10000"),24), 0, spannable.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+            }
+            else
+                spannable.setSpan(new BulletSpan(leadingMargin, Color.GRAY), 0, spannable.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+            sb.append(spannable);
+        }
+        return sb;
+    }
+
+}
