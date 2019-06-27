15d087ac69332edab729d0e313fcd58cb0d88b22
alexsalupa97 <alexsalupa97@gmail.com>
2019-06-27 10:41:14 +0300

    ascundere keyboard signup

--- app/src/main/java/ro/alexsalupa97/bloodbank/Activitati/SignupActivity.java
+++ app/src/main/java/ro/alexsalupa97/bloodbank/Activitati/SignupActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.InputMethodManager;
 
 import ro.alexsalupa97.bloodbank.AdaptoareFragmente.AdaptorFragmenteSignup;
 import ro.alexsalupa97.bloodbank.R;
@@ -26,6 +27,30 @@ public class SignupActivity extends AppCompatActivity {
         adaptor = new AdaptorFragmenteSignup(getSupportFragmentManager());
         viewPager = (ViewPager) findViewById(R.id.loginViewpager);
         viewPager.setAdapter(adaptor);
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+//                final InputMethodManager imm = (InputMethodManager) getSystemService(
+//                        Context.INPUT_METHOD_SERVICE);
+//                imm.hideSoftInputFromWindow(viewPager.getWindowToken(), 0);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float offset, int offsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    if (viewPager.getCurrentItem() == 0 || viewPager.getCurrentItem()==1) {
+                        // Hide the keyboard.
+                        ((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
+                                .hideSoftInputFromWindow(viewPager.getWindowToken(), 0);
+                    }
+                }
+            }
+        });
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.loginTab);
         tabLayout.setupWithViewPager(viewPager);
